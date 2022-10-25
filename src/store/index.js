c9fd1f75d672cb82f05e76829f33362189415859
Boaz Dejene <boazdeju@gmail.com>
2022-10-25 10:39:57 +0300

    latest

--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,6 @@ import { configureStore } from '@reduxjs/toolkit';
 // project import
 import reducers from './reducers';
 
-// ==============================|| REDUX TOOLKIT - MAIN STORE ||============================== //
-
 const store = configureStore({
     reducer: reducers
 });
