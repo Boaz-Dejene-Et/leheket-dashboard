d13680079dee4897a2a55ce0b0b9529212b2d04d
Boaz Dejene <boazdeju@gmail.com>
2022-11-24 23:09:33 +0300

    final

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-// project import
-// import Routes from 'routes';
+import React, { lazy } from 'react';
 import ThemeCustomization from 'themes';
 import ScrollTop from 'components/ScrollTop';
 import { Routes, Route, Navigate } from "react-router-dom";
 import MainLayout from 'layout/MainLayout';
 
-import { lazy, useEffect } from 'react';
-import { ToastContainer, toast } from 'react-toastify';
+// import Toast message
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 // project import
@@ -14,7 +13,7 @@ import Loadable from 'components/Loadable';
 import MinimalLayout from 'layout/MinimalLayout';
 
 // redux
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 
 // render - dashboard
 const DashboardDefault = Loadable(lazy(() => import('pages/dashboard')));
@@ -23,15 +22,6 @@ const Users = Loadable(lazy(() => import('pages/users')));
 const Notification = Loadable(lazy(() => import('pages/notification')));
 const AddBook = Loadable(lazy(() => import('pages/add-book')));
 const Books = Loadable(lazy(() => import('pages/books')));
-
-// render - sample page
-const SamplePage = Loadable(lazy(() => import('pages/extra-pages/SamplePage')));
-
-// render - utilities
-const Typography = Loadable(lazy(() => import('pages/components-overview/Typography')));
-const Color = Loadable(lazy(() => import('pages/components-overview/Color')));
-const Shadow = Loadable(lazy(() => import('pages/components-overview/Shadow')));
-const AntIcons = Loadable(lazy(() => import('pages/components-overview/AntIcons')));
 
 // render - login
 const AuthLogin = Loadable(lazy(() => import('pages/authentication/Login')));
@@ -43,7 +33,6 @@ const App = () => {
         <ThemeCustomization>
             <ScrollTop>
                 <ToastContainer />
-                {/* <Routes /> */}
                 <Routes>
                     {isAuth ? (
                         <Route path="/" element={<MainLayout />}>
@@ -53,11 +42,6 @@ const App = () => {
                             <Route path="users" element={<Users />} />
                             <Route path="notification" element={<Notification />} />
                             <Route path="add_book" element={<AddBook />} />
-                            <Route path="color" element={<Color />} />
-                            <Route path="sample-page" element={<SamplePage />} />
-                            <Route path="shadow" element={<Shadow />} />
-                            <Route path="typography" element={<Typography />} />
-                            <Route path="icons/ant" element={<AntIcons />} />
                         </Route>
                     ) : (
                         <>
